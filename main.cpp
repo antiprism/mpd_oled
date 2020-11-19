025c31192ca648f4dfecf133432235540205d5b4
Adrian Rossiter <adrian@antiprism.com>
2020-11-19 20:29:02 +0100

    Fix build, remove use of C++11 feature

--- main.cpp
+++ main.cpp
@@ -441,7 +441,7 @@ void draw_spect_display(ArduiPi_OLED &display, const display_info &disp_info)
 
 void draw_display(ArduiPi_OLED &display, const display_info &disp_info)
 {
-  auto state = disp_info.status.get_state();
+  mpd_state state = disp_info.status.get_state();
   if (state == MPD_STATE_UNKNOWN || state == MPD_STATE_STOP ||
       (state == MPD_STATE_PAUSE && disp_info.pause_screen == 's'))
     draw_clock(display, disp_info);
